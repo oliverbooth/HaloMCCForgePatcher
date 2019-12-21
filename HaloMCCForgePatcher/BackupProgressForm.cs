2606c9a449ef7cb443de06b62f6973eea445a8ed
Oliver Booth <me@olivr.me>
2019-12-21 02:26:06 +0000

    ✨ Add backup mechanic and progress form

---
+++ HaloMCCForgePatcher/BackupProgressForm.cs
@@ -0,0 +1,58 @@
+﻿namespace HaloMCCForgePatcher
+{
+    #region Using Directives
+
+    using System.Drawing;
+    using System.Windows.Forms;
+    using Helpers;
+
+    #endregion
+
+    public sealed class BackupProgressForm : Form
+    {
+        public BackupProgressForm()
+        {
+            this.ClientSize      = new Size(305, 40);
+            this.Text            = "Creating backup";
+            this.StartPosition   = FormStartPosition.CenterScreen;
+            this.Icon            = Resources.HaloMCC_v1;
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            this.MaximizeBox     = false;
+            this.SizeGripStyle   = SizeGripStyle.Hide;
+
+            this.PercentageProgressBar = new ProgressBar
+            {
+                Dock    = DockStyle.Fill,
+                Maximum = 100,
+                Style   = ProgressBarStyle.Continuous
+            };
+
+            this.Controls.Add(this.PercentageProgressBar);
+
+            this.Closing += (sender, args) =>
+            {
+                args.Cancel = !MsgBoxHelpers.ConfirmYesNo(
+                    "Are you sure you want to cancel the backup and proceed with patching?",
+                    "Confirm Backup Cancel",
+                    () =>
+                    {
+                        FileHelpers.CancelBackup();
+                        return true;
+                    },
+                    () =>
+                    {
+                        // do nothing
+                    });
+            };
+        }
+
+        #region Properties
+
+        /// <summary>
+        /// Gets the progress bar control in this form.
+        /// </summary>
+        public ProgressBar PercentageProgressBar { get; }
+
+        #endregion
+    }
+}
---
+++ HaloMCCForgePatcher/Helpers/FileHelpers.cs
@@ -0,0 +1,76 @@
+﻿namespace HaloMCCForgePatcher.Helpers
+{
+    #region Using Directives
+
+    using System;
+    using System.IO;
+    using System.Threading.Tasks;
+
+    #endregion
+
+    internal static class FileHelpers
+    {
+        private static bool cancelBackupCreation = false;
+
+        /// <summary>
+        /// Asynchronously creates a backup.
+        /// </summary>
+        /// <param name="sourcePath"></param>
+        /// <param name="percentageChangeCallback"></param>
+        /// <returns></returns>
+        public static async Task CreateBackupAsync(string sourcePath, Action<double> percentageChangeCallback)
+        {
+            cancelBackupCreation = false;
+            sourcePath           = Path.GetFullPath(sourcePath);
+
+            string destinationPath = Path.GetFullPath($"{sourcePath}.bak");
+
+            if (!File.Exists(sourcePath))
+            {
+                return;
+            }
+
+            if (File.Exists(destinationPath))
+            {
+                File.Delete(destinationPath);
+            }
+
+            byte[] buffer = new byte[1048576]; // 1 MiB buffer
+
+            await using FileStream source =
+                new FileStream(sourcePath, FileMode.Open, FileAccess.Read);
+            await using FileStream destination =
+                new FileStream(destinationPath, FileMode.CreateNew, FileAccess.Write);
+
+            long length     = source.Length;
+            long totalBytes = 0;
+            int  currentBlockSize;
+
+            while ((currentBlockSize = source.Read(buffer, 0, buffer.Length)) > 0)
+            {
+                totalBytes += currentBlockSize;
+                double percentage = totalBytes * 100.0 / length;
+
+                destination.Write(buffer, 0, currentBlockSize);
+                percentageChangeCallback?.Invoke(percentage);
+
+                if (!cancelBackupCreation)
+                {
+                    continue;
+                }
+
+                // discard backup
+                source.Close();
+                destination.Close();
+                File.Delete(destinationPath);
+                cancelBackupCreation = false;
+                break;
+            }
+        }
+
+        public static void CancelBackup()
+        {
+            cancelBackupCreation = true;
+        }
+    }
+}
